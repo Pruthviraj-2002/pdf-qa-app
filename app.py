ab635d77fdd4b57799de3da0ce7c9abbb8352179
Pruthvi Rallapally <pruthvirallapally2002@gmail.com>
2025-04-07 21:32:08 +0530

    Fix: Load test.pdf from static directory

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def home():
 
 def extract_text_from_pdf(pdf_path):
     print(f"Trying to open: {pdf_path}")
-    doc = fitz.open("static/test.pdf")
+    doc = fitz.open("pdf", pdf_path)
 
     text = ""
     for page in doc:
